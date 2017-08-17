62a24c94c51c256f827c73b026dea669867e488a
antu xie <antuxie@gmail.com>
2017-08-17 00:09:38 -0500

    changed silent.html

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -14,8 +14,8 @@ export const AUTH_CONFIG: AuthConfig = {
   CLIENT_ID: 'laUldWlHyZnGNji4C6eRzT9l98sD59se',
   CLIENT_DOMAIN: 'eddiezha.auth0.com', // e.g., kmaida.auth0.com
   AUDIENCE: 'http://localhost:8083/api/', // e.g., http://localhost:8083/api/
-  REDIRECT: `${ENV.BASE_URI}/silent`,
-  SILENT_REDIRECT: 'http://localhost:8083/silent', // ${ENV.BASE_URI}/silent on PRODUCTION // 'http://localhost:8083/silent' on DEV
+  REDIRECT: `${ENV.BASE_URI}/callback`,
+  SILENT_REDIRECT: 'http://localhost:8083/silent', // ${ENV.BASE_URI}/silent on PRODUCTION // 'http://localhost:8083/silent'
   SCOPE: 'openid profile',
   NAMESPACE: 'http://myapp.com/roles'
 };
