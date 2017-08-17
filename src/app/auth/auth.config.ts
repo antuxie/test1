8f46b4cd7b7b9a787242f20f534d58d99112999c
antu xie <antuxie@gmail.com>
2017-08-17 00:07:42 -0500

    first commit

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -11,11 +11,11 @@ interface AuthConfig {
 };
 
 export const AUTH_CONFIG: AuthConfig = {
-  CLIENT_ID: '[AUTH0_CLIENT_ID]',
-  CLIENT_DOMAIN: '[AUTH0_CLIENT_DOMAIN]', // e.g., kmaida.auth0.com
-  AUDIENCE: '[YOUR_AUTH0_API_AUDIENCE]', // e.g., http://localhost:8083/api/
-  REDIRECT: `${ENV.BASE_URI}/callback`,
-  SILENT_REDIRECT: 'http://localhost:8083/silent', // ${ENV.BASE_URI}/silent on PRODUCTION
+  CLIENT_ID: 'laUldWlHyZnGNji4C6eRzT9l98sD59se',
+  CLIENT_DOMAIN: 'eddiezha.auth0.com', // e.g., kmaida.auth0.com
+  AUDIENCE: 'http://localhost:8083/api/', // e.g., http://localhost:8083/api/
+  REDIRECT: `${ENV.BASE_URI}/silent`,
+  SILENT_REDIRECT: 'http://localhost:8083/silent', // ${ENV.BASE_URI}/silent on PRODUCTION // 'http://localhost:8083/silent' on DEV
   SCOPE: 'openid profile',
   NAMESPACE: 'http://myapp.com/roles'
 };
