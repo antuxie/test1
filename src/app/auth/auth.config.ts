a1d23a648424e562722c1ba5d79512e5a160a029
antu xie <antuxie@gmail.com>
2017-08-18 22:48:14 -0500

    modified auth.config.js

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -11,8 +11,8 @@ interface AuthConfig {
 };
 
 export const AUTH_CONFIG: AuthConfig = {
-  CLIENT_ID: 'laUldWlHyZnGNji4C6eRzT9l98sD59se',
-  CLIENT_DOMAIN: 'eddiezha.auth0.com', // e.g., kmaida.auth0.com
+  CLIENT_ID: 'lGVngmMe2S78tEFRJVrOcssDcIeDOsYnc',
+  CLIENT_DOMAIN: 'eddiezhazha.auth0.com', // e.g., kmaida.auth0.com
   AUDIENCE: 'http://localhost:8083/api/', // e.g., http://localhost:8083/api/
   REDIRECT: `${ENV.BASE_URI}/callback`,
   SILENT_REDIRECT: 'http://localhost:8083/silent', // ${ENV.BASE_URI}/silent on PRODUCTION // 'http://localhost:8083/silent'
